0685b2499116246a684e0b58a1a0aa6395509d72
Mahbub Mithil <mahbubmithil776@gmail.com>
2026-05-03 00:17:24 +0600

    fix: cors and port config

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://wifi-managment-system.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
