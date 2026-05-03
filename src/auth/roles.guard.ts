132d1807572b119b2684bffd98a35f9c31d33631
Mahbub Mithil <mahbubmithil776@gmail.com>
2026-05-03 22:26:33 +0600

    chore: remove backend files from main

--- src/auth/roles.guard.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-
-@Injectable()
-export class RolesGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
-    const { user } = context.switchToHttp().getRequest();
-    return user && user.role === 'admin';
-  }
-}
